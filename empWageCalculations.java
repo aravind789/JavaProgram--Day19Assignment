a30c6d7f858388a0c912bdf42fcf9cbd2f694180
aravind789 <aravindkris89@gmail.com>
2020-07-16 13:40:59 +0530

    Added: Steps to check employee is present or not

--- empWageCalculations.java
+++ empWageCalculations.java
@@ -1,1 +1,12 @@
-Welcome
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+		int attendence = (int) Math.floor(Math.random()*10)%2;
+		if(attendence == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
+	}
+
+}
