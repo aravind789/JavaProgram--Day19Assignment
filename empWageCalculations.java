0d29dfb41f44c6a3d3a7c10488920acaec5cd4ab
aravind789 <aravindkris89@gmail.com>
2020-07-16 13:52:49 +0530

    Added: Steps to calculate Employee wage

--- empWageCalculations.java
+++ empWageCalculations.java
@@ -1,11 +1,18 @@
 public class EmpWageComputation {
+static int attendence = (int) (Math.floor(Math.random()*10)%2)+1;
+	final static int IS_PART_TIME=1;
+	final static int IS_FULL_TIME=2;
+	final static int WAGE_PER_HOUR=20; 
+	static int workHours;
+	static int dailyWage;
 
 	public static void main(String[] args) {
-		int attendence = (int) Math.floor(Math.random()*10)%2;
-		if(attendence == 1)
-			System.out.println("Employee is Present");
+		if(attendence == 2 )
+			workHours=8;
 		else
-			System.out.println("Employee is Absent");
+			workHours=0;
+		dailyWage=workHours*WAGE_PER_HOUR;
+		System.out.println("Daily Wage of Employee  "+dailyWage);
 
 	}
 
