7d8214507489532c37742ee613310927f5fafb00
aravind789 <aravindkris89@gmail.com>
2020-07-16 14:33:14 +0530

    Added: Steps to calculate wage for a month

--- empWageCalculations.java
+++ empWageCalculations.java
@@ -1,12 +1,8 @@
-public class EmpWageComputation {
-static int attendence = (int) (Math.floor(Math.random()*10)%3);
+class Attendance{
 	final static int IS_PART_TIME=1;
 	final static int IS_FULL_TIME=2;
-	final static int WAGE_PER_HOUR=20; 
-	static int workHours;
-	static int dailyWage;
-
-	public static void main(String[] args) {
+	int workHours;
+	public int getWorkHours(int attendence){
 		switch(attendence) {
 			case IS_FULL_TIME:
 				workHours=8;
@@ -17,9 +13,34 @@ static int attendence = (int) (Math.floor(Math.random()*10)%3);
 			default:
 				workHours=0;
 		}
-		dailyWage=workHours*WAGE_PER_HOUR;
-		System.out.println("Daily Wage of Employee  "+dailyWage);
+		return workHours;
 
 	}
 
 }
+public class EmpWageComputation {
+	final static int TOTAL_WORKING_DAYS=20;
+	final static int WAGE_PER_HOUR=20; 
+	static int attendence;
+	static int oneDayWorkHours;
+	static int dailyWage;
+	static int totalWagesPerMonth=0;
+	static int workingDays=0;
+	public static void main(String[] args) {
+
+		Attendance hrs = new Attendance();
+		while(workingDays<=TOTAL_WORKING_DAYS)
+		{
+			workingDays++;
+			attendence = (int) (Math.floor(Math.random()*10))%3;
+			oneDayWorkHours=hrs.getWorkHours(attendence);
+			dailyWage=oneDayWorkHours*WAGE_PER_HOUR;
+			totalWagesPerMonth+=dailyWage;
+			System.out.println("DailyWage on "+workingDays+" th Day= "+dailyWage);
+		}
+		System.out.println("Total Wages Per Month= "+totalWagesPerMonth);
+
+	}
+
+}
+
