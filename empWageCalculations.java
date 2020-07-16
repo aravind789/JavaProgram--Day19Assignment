66410dc5e3a2ed297c9233d7a220ba1e8dbc321c
aravind789 <aravindkris89@gmail.com>
2020-07-16 14:23:29 +0530

    Added: switch case insted of if-else

--- empWageCalculations.java
+++ empWageCalculations.java
@@ -1,5 +1,5 @@
 public class EmpWageComputation {
-static int attendence = (int) (Math.floor(Math.random()*10)%2)+1;
+static int attendence = (int) (Math.floor(Math.random()*10)%3);
 	final static int IS_PART_TIME=1;
 	final static int IS_FULL_TIME=2;
 	final static int WAGE_PER_HOUR=20; 
@@ -7,10 +7,16 @@ static int attendence = (int) (Math.floor(Math.random()*10)%2)+1;
 	static int dailyWage;
 
 	public static void main(String[] args) {
-		if(attendence == 2 )
-			workHours=8;
-		else
-			workHours=4;
+		switch(attendence) {
+			case IS_FULL_TIME:
+				workHours=8;
+		break;
+			case IS_PART_TIME
+				workHours=4;
+		break;
+			default:
+				workHours=0;
+		}
 		dailyWage=workHours*WAGE_PER_HOUR;
 		System.out.println("Daily Wage of Employee  "+dailyWage);
 
